18c3ab17ca4a13f5b97f79e1f63f99614c965164
Suat OĞUL <suatogul@gmail.com>
2022-09-23 15:14:36 +0200

    card-item component created

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cards.component.scss']
 })
 export class CardsComponent implements OnInit {
+  cardItem={
+    name:"Suat Ogul",
+    title:"Frontend Designer",
+    phone:"077 789 87 87",
+    email:"example@gmail.com",
+    address:"Bern, Switzerland"
+  }
 
   constructor() { }
 
